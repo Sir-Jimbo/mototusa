f9cfa36ec65a05a0987f13e3f6171f1aaebe86d5
Alexey Golodenko <64161100+Sir-Jimbo@users.noreply.github.com>
2022-11-27 18:14:54 +0200

    add_adaptive__50%

--- js/sliders.js
+++ js/sliders.js
@@ -3,7 +3,32 @@ const sale = new Swiper('.sale__slider', {
    slidesPerView: 1.325,
    spaceBetween: 10,
    centeredSlides: true,
-   //loop: true,
+   loop: true,
+   watchOverflow: true,
+   //loopedSlides: 1,
+
+   breakpoints: {
+      768: {
+         slidesPerView: 2.5,
+      },
+
+      960: {
+         slidesPerView: 3.5,
+      },
+
+      1024: {
+         slidesPerView: 4,
+         spaceBetween: 35,
+         centeredSlides: false,
+
+      },
+
+      1440: {
+         slidesPerView: 4,
+         spaceBetween: 70,
+         centeredSlides: false,
+      }
+   },
 
    // Navigation arrows
    navigation: {
@@ -16,10 +41,30 @@ const sale = new Swiper('.sale__slider', {
 
 const popular = new Swiper('.popular__slider', {
    // Optional parameters
-   slidesPerView: 1.325,
+   slidesPerView: 2.2,
    spaceBetween: 10,
    centeredSlides: true,
-   //loop: true,
+   loop: true,
+
+   breakpoints: {
+      568: {
+         slidesPerView: 4.5,
+      },
+
+      960: {
+         slidesPerView: 5.5,
+      },
+
+      1024: {
+         slidesPerView: 6,
+         centeredSlides: false,
+      },
+
+      1440: {
+         slidesPerView: 10,
+         centeredSlides: false,
+      },
+   },
 
    // Navigation arrows
    navigation: {
@@ -32,10 +77,30 @@ const popular = new Swiper('.popular__slider', {
 
 const consumables = new Swiper('.consumables__slider', {
    // Optional parameters
-   slidesPerView: 1.325,
+   slidesPerView: 2.2,
    spaceBetween: 10,
    centeredSlides: true,
-   //loop: true,
+   loop: true,
+
+   breakpoints: {
+      568: {
+         slidesPerView: 4.5,
+      },
+
+      960: {
+         slidesPerView: 5.5,
+      },
+
+      1024: {
+         slidesPerView: 6,
+         centeredSlides: false,
+      },
+
+      1440: {
+         slidesPerView: 10,
+         centeredSlides: false,
+      },
+   },
 
    // Navigation arrows
    navigation: {
@@ -48,20 +113,22 @@ const consumables = new Swiper('.consumables__slider', {
 
 const blog = new Swiper('.blog__slider', {
    // Optional parameters
-   spaceBetween: 10,
-   centeredSlides: true,
-   //loop: true,
-
+   watchOverflow: true,
+   breakpoints: {
+      568: {
+         slidesPerView: 2,
+      },
+      1024: {
+         slidesPerView: 3,
+      },
+      1440: {
+         slidesPerView: 4,
+      },
+   },
    pagination: {
       el: '.swiper-pagination',
       clickable: true
    },
-   // Navigation arrows
-   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-   },
-
 
 });
 
@@ -69,7 +136,7 @@ const photogalery = new Swiper('.photogalery__slider', {
    // Optional parameters
    spaceBetween: 10,
    centeredSlides: true,
-   //loop: true,
+   loop: true,
 
    pagination: {
       el: '.swiper-pagination',
